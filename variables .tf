563377e2fdc866b818cbac58ab994f29277ae125
Prudhvi Garikapati <gsvprudhvi2334@gmail.com>
2021-10-11 10:30:42 -0400

    Initial commit

---
+++ variables .tf
@@ -0,0 +1,36 @@
+variable "project" {
+  default = "unique-atom-328403"
+}
+
+variable "region" {
+  default = "us-east1"
+}
+
+variable "zone" {
+  default = "us-east1-d"
+}
+
+variable "cluster" {
+  default = "terraform-exercise"
+}
+
+variable "credentials" {
+  default = "unique-atom-328403-8a0f1cb37776.json"
+}
+
+variable "kubernetes_min_ver" {
+  default = "latest"
+}
+
+variable "kubernetes_max_ver" {
+  default = "latest"
+}
+
+variable "app_name" {
+ default = "app1"
+}
+
+variable "machine_type" {
+ default = "e2-micro"
+}
+
